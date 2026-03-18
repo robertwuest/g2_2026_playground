1cba6c74949031d033b5a473ab198b9f5eef77f8
Yannic Singer <Yannic.Singer@gmx.net>
2026-03-18 12:33:09 +0100

    added magic methods

--- Mitarbeiter.py
+++ Mitarbeiter.py
@@ -1,18 +1,27 @@
 from Kunden import Kunde
 
+
 class Mitarbeiter:
-    def __init__(self, name, alter, position):
+    def __init__(self, id, name, alter, position):
+        self.mitarbeiter_id = id
         self.name = name
         self.alter = alter
         self.position = position
 
     def __str__(self):
         return f"{self.name}, {self.alter} Jahre alt, Position: {self.position}"
-    
+
+    def __eq__(self, mitarbeiter2: "Mitarbeiter"):
+        if self.mitarbeiter_id == mitarbeiter2.mitarbeiter_id:
+            return True
+        else:
+            return False
+
+
 class Kundenbetreuung:
     def __init__(self, mitarbeiter, kunde: Kunde):
         self.mitarbeiter = mitarbeiter
         self.kunde = kunde
 
-    def betreue_kunden(self):
-        print(f"{self.mitarbeiter.name} betreut {self.kunde}.")
+    def __str__(self):
+        return f"{self.mitarbeiter.name} betreut {self.kunde}."
--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from Kunden import Kunde
 from Mitarbeiter import Mitarbeiter, Kundenbetreuung
 
-mitarbeiter_1 = Mitarbeiter("Alice", 30, "Bibliothekarin")
-mitarbeiter_2 = Mitarbeiter("Bob", 25, "Ferienjöbbler")
-mitarbeiter_3 = Mitarbeiter("Charlie", 35, "Sekretär")
+mitarbeiter_1 = Mitarbeiter(1232, "Alice", 30, "Bibliothekarin")
+mitarbeiter_2 = Mitarbeiter(1233, "Bob", 25, "Ferienjöbbler")
+mitarbeiter_3 = Mitarbeiter(1234, "Charlie", 35, "Sekretär")
 
-kunde_1 = Kunde(1, "Max", "Mustermann", "max.mustermann@example.com", "123456789", "Musterstraße 1, 12345 Musterstadt")
-kunde_2 = Kunde(2, "Erika", "Musterfrau", "erika.musterfrau@example.com", "987654321", "Musterstraße 2, 12345 Musterstadt")
+kunde_1 = Kunde(
+    1,
+    "Max",
+    "Mustermann",
+    "max.mustermann@example.com",
+    "123456789",
+    "Musterstraße 1, 12345 Musterstadt",
+)
+kunde_2 = Kunde(
+    2,
+    "Erika",
+    "Musterfrau",
+    "erika.musterfrau@example.com",
+    "987654321",
+    "Musterstraße 2, 12345 Musterstadt",
+)
 
 betreuung_1 = Kundenbetreuung(mitarbeiter_3, kunde_1)
 betreuung_2 = Kundenbetreuung(mitarbeiter_2, kunde_2)
 
+print(mitarbeiter_1 == mitarbeiter_2)
+print(mitarbeiter_1 == mitarbeiter_1)
