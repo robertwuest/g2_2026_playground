6c9859762768df1c5bd3a912e3bd51c588ad3378
Arkic <armin.bjw@gmail.com>
2026-03-25 11:36:27 +0100

    Verknüpfung von Person und Kunde

---
+++ Person.py
@@ -0,0 +1,50 @@
+from Ausleihe import Ausleihe
+from Person import Person
+ 
+ 
+class Kunde(Person):
+    def __init__(
+        self,
+        kunden_id,
+        vorname,
+        nachname,
+        geburtstag,
+        email,
+        telefon,
+        strassenname,
+        strassennummer,
+        plz,
+        ort,
+    ):
+        self.kunden_id = kunden_id
+        super().__init__(
+            vorname,
+            nachname,
+            geburtstag,
+            email,
+            telefon,
+            strassenname,
+            strassennummer,
+            plz,
+            ort,
+        )
+ 
+    def anzeigen(self):
+        print(f"Kunden-ID: {self.kunden_id}")
+        print(f"Name: {self.vorname} {self.nachname}")
+        print(f"E-Mail: {self.email}")
+        print(f"Telefon: {self.telefon}")
+        print(f"Adresse: {self.adresse}")
+        print(f"Aktiv: {self.aktiv}")
+ 
+    def deaktivieren(self):
+        self.aktiv = False
+ 
+ 
+class Buchausleihe:
+    def __init__(self, ausleihe: Ausleihe, kunde: Kunde):
+        self.kunde = kunde
+        self.ausleihe = ausleihe
+ 
+    def kunde_leiht_aus(self):
+        print(f"{self.Kunde.vorname} {self.Kunde.nachname} leiht aus {self.ausleihe}.")
