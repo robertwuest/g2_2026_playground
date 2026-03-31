ca1e4cb635a3aee022508f85b47629adbab3d9ca
Your Name <you@example.com>
2026-03-31 18:04:39 +0200

    Merged class Person from main into Person from feature.

--- Person.py
+++ Person.py
@@ -3,9 +3,27 @@ from dateutil.relativedelta import relativedelta
 
 
 class Person:
-    def __init__(self, name: str, date_of_birth: str):
-        self.name = name
+    def __init__(
+        self,
+        vorname,
+        nachname,
+        date_of_birth,
+        email,
+        telefon,
+        strassenname,
+        strassennummer,
+        plz,
+        ort,
+    ):
+        self.vorname = vorname
+        self.nachname = nachname
         self.date_of_birth = datetime.strptime(date_of_birth, '%Y-%m-%d')
+        self.email = email
+        self.telefon = telefon
+        self.strassenname = strassenname
+        self.strassennummer = strassennummer
+        self.plz = plz
+        self.ort = ort        
 
     def get_age(self) -> int:
         return relativedelta(datetime.now(), self.date_of_birth).years
